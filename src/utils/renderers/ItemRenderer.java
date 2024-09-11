2b952b7f3f6fc7acf37ceef972fd82d82cef7d37
Victor <victorcaetanosouto@hotmail.com>
2024-09-11 17:06:29 -0300

    ItemRenderer Implementation

--- src/utils/renderers/ItemRenderer.java
+++ src/utils/renderers/ItemRenderer.java
@@ -1,4 +1,28 @@
 package utils.renderers;
 
-public class ItemRenderer {
+import dao.Customer;
+import dao.Product;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ItemRenderer extends DefaultListCellRenderer {
+    @Override
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        if (value instanceof Product product) {
+            setText(product.getName() + " (ID: " + product.getId() + ")");
+            if (product.getId() == 0) {
+                setText(product.getName());
+            }
+        }
+        if (value instanceof Customer customer) {
+            setText(customer.getName() + " (ID: " + customer.getId() + ")");
+            if (customer.getId() == 0) {
+                setText(customer.getName());
+            }
+        }
+        return this;
+    }
+
 }
